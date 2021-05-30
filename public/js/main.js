a9efd35cc0dccbeb994ca6384afb75e1fab70c3f
Marcel <maschmidt.dev@gmail.com>
2021-05-30 02:08:45 +0200

    Basic game functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,197 @@
 (function () {
+    //  ===================== SERVER ==========================
+
     const socket = io();
-    var el;
 
     socket.on('message', (message) => {
         console.log(message);
     });
 
-    socket.on('time', function (timeString) {
-        el = document.getElementById('server-time');
-        el.innerHTML = 'Server time: ' + timeString;
+    socket.on('player 1', function () {
+        console.log('You are now player 1!');
+        $('#player').html(
+            '<h1>You are <span class="player-1">Player 1</span>. <span id="turn"> Its your turn!</span></h1>'
+        );
     });
+
+    socket.on('player 2', function () {
+        console.log('You are now player 2!');
+        $('#player').html(
+            '<h1>You are <span class="player-2">Player 2</span>. <span id="turn"> Its your turn!</span></h1>'
+        );
+    });
+
+    socket.on('turn', function (turn) {
+        if (turn) {
+            $('#turn').addClass('turn');
+        } else {
+            $('#turn').removeClass('turn');
+        }
+    });
+
+    socket.on('placeTile', ({ index, player }) => {
+        console.log('index:', index, 'player:', player);
+        $('.position')
+            .eq(index)
+            .addClass('player-' + player);
+    });
+
+    //  ===================== GAME ==========================
+
+    var currentPlayer = 1;
+    var $board = $('.connect-four');
+    var $columns = $board.find('.column');
+
+    var gameFinished = false;
+
+    $columns.on('click', function () {
+        // if (gameFinished) {
+        //     return;
+        // }
+        var $columnPositions = $(this).find('.position');
+        var colIndex = $columns.index(this);
+        // var rowIndex = placeTile($columnPositions);
+
+        socket.emit('click', colIndex);
+
+        // if the column is already full
+        //     if (rowIndex < 0) {
+        //         return;
+        //     }
+
+        //     var $rowPositions = getRowPositions(rowIndex);
+        //     var $topDiagonalPositions = getTopDiagonalPositions(rowIndex, colIndex);
+        //     var $tbottomDiagonalPositions = getBottomDiagonalPositions(
+        //         rowIndex,
+        //         colIndex
+        //     );
+        //     var checkPositions = [];
+        //     checkPositions.push($columnPositions);
+        //     checkPositions.push($rowPositions);
+        //     checkPositions.push($topDiagonalPositions);
+        //     checkPositions.push($tbottomDiagonalPositions);
+
+        //     if (!checkVictory(checkPositions, 0)) {
+        //         switchPlayer();
+        //     } else {
+        //         gameFinished = true;
+        //         $('#player').append('<button class="btn">Play again?</button>');
+        //     }
+    });
+
+    // $('#player').on('click', 'button', () => reset());
+
+    // function getTopDiagonalPositions(row, col) {
+    //     var $diag = $();
+    //     var $positions = $('.position');
+
+    //     // Get top left position of the diagonal
+    //     while (row > 0 && col > 0) {
+    //         row--;
+    //         col--;
+    //     }
+    //     // add positions diagonally down
+    //     while (row <= 5 && col <= 6) {
+    //         $diag = $diag.add($positions.eq(col * 6 + row));
+    //         row++;
+    //         col++;
+    //     }
+    //     return $diag;
+    // }
+
+    // function getBottomDiagonalPositions(row, col) {
+    //     var $diag = $();
+    //     var $positions = $('.position');
+
+    //     // Get bottom left position of the diagonal
+    //     while (row < 5 && col > 0) {
+    //         row++;
+    //         col--;
+    //     }
+    //     // add positions diagonally up
+    //     while (row >= 0 && col <= 6) {
+    //         $diag = $diag.add($positions.eq(col * 6 + row));
+    //         row--;
+    //         col++;
+    //     }
+    //     return $diag;
+    // }
+
+    // function getRowPositions(rowIndex) {
+    //     var $row = $();
+
+    //     for (var i = 0; i < $columns.length; i++) {
+    //         var $column = $columns.eq(i);
+    //         var $columnPositions = $column.find('.position');
+    //         var $pos = $columnPositions.eq(rowIndex);
+    //         $row = $row.add($pos);
+    //     }
+
+    //     return $row;
+    // }
+
+    // function checkVictory(checkPositions, index) {
+    //     if (index < checkPositions.length) {
+    //         var count = 0;
+    //         var winPositions = [];
+
+    //         for (var i = 0; i < checkPositions[index].length; i++) {
+    //             var $current = checkPositions[index].eq(i);
+    //             if ($current.hasClass('player-' + currentPlayer)) {
+    //                 winPositions.push($current);
+    //                 count++;
+    //                 if (count >= 4) {
+    //                     $('#player h1').text($('#player h1').text() + ' wins!');
+    //                     for ($w of winPositions) {
+    //                         $w.addClass('win');
+    //                     }
+    //                     return true;
+    //                 }
+    //             } else {
+    //                 count = 0;
+    //                 winPositions = [];
+    //             }
+    //         }
+
+    //         return checkVictory(checkPositions, index + 1);
+    //     } else {
+    //         return false;
+    //     }
+    // }
+
+    // function placeTile($columnPositions) {
+    //     for (var i = $columnPositions.length - 1; i >= 0; i--) {
+    //         var $current = $columnPositions.eq(i);
+    //         var isTaken =
+    //             $current.hasClass('player-1') || $current.hasClass('player-2');
+
+    //         if (!isTaken) {
+    //             $current.addClass('player-' + currentPlayer);
+    //             return i;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+
+    // function switchPlayer() {
+    //     if (currentPlayer === 1) {
+    //         currentPlayer = 2;
+    //     } else {
+    //         currentPlayer = 1;
+    //     }
+    //     $('#player span').html('' + currentPlayer);
+    //     $('#player h1').toggleClass('player-1');
+    //     $('#player h1').toggleClass('player-2');
+    // }
+
+    // function reset() {
+    //     $('#player').html('<h1 class="player-1">Player <span>1</span></h1>');
+    //     currentPlayer = 1;
+    //     $positions = $('.position');
+    //     $positions.removeClass('player-1');
+    //     $positions.removeClass('player-2');
+    //     $('.win').removeClass('win');
+    //     gameFinished = false;
+    // }
 })();
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
     const http = require('http');
     const express = require('express');
     const socketio = require('socket.io');
+    const PORT = process.env.PORT || 3000;
 
     const app = express();
     const server = http.createServer(app);
@@ -11,23 +12,217 @@
     // Set static folder
     app.use(express.static(path.join(__dirname, 'public')));
 
+    var clients = [];
+    var currentPlayer;
+    var gameBoard = [];
+    var gameFinished;
+    init();
+
     // Run when client connects
     io.on('connection', (socket) => {
         // Welcome current user
         socket.emit('message', 'Welcome!');
 
+        // Place client in array
+        if (clients.length > 0 && clients[0] === 'waiting') {
+            clients[0] = socket.id;
+            socket.emit('player 1');
+            socket.emit('turn', currentPlayer === 1);
+        } else {
+            clients.push(socket.id);
+            if (clients.length === 1) {
+                socket.emit('player 1');
+                socket.emit('turn', currentPlayer === 1);
+            } else if (clients.length === 2) {
+                socket.emit('player 2');
+                socket.emit('turn', currentPlayer === 2);
+            }
+        }
+
         // Broadcast when a user connects
-        socket.broadcast.emit('message', 'A user jas joined the chat');
+        socket.broadcast.emit('message', 'A user jas joined the game');
 
         //  Runs when client disconnects
         socket.on('disconnect', () => {
-            io.emit('message', 'A user has left the chat');
+            io.emit('message', 'A user has left the game');
+            // If player 1 disconnects
+            if (clients.indexOf(socket.id) === 0) {
+                // If there are spectators
+                if (clients.length >= 3) {
+                    clients[0] = clients[2]; // Make first spectator (index 2) new player 1
+                    socket.to(clients[0]).emit('player 1');
+                    socket.to(clients[0]).emit('turn', currentPlayer === 1);
+                    clients.splice(2, 1); // Remove previous spectator position from clients array
+                    // If there are just the two players
+                } else if ((clients.length = 2)) {
+                    clients[0] = 'waiting'; // reserve Player 1 spot for the next connection
+                    // just empty array if player 1 was the only client
+                } else {
+                    clients = [];
+                }
+                // If player 2 disconnects
+            } else if (clients.indexOf(socket.id) === 1) {
+                // If player 1 pending
+                if (clients[0] === 'waiting') {
+                    clients = []; // reset clients
+                    //  If there are spectators
+                } else if (clients.length >= 3) {
+                    clients[1] = clients[2]; // Make first spectator (index 2) new player 2
+                    socket.to(clients[1]).emit('player 2');
+                    socket.to(clients[1]).emit('turn', currentPlayer === 2);
+                    clients.splice(2, 1); // Remove previous spectator position from clients array
+                }
+            } else {
+                clients.splice(clients.indexOf(socket.id, 1));
+            }
+            clients = clients.filter((client) => client !== socket.id);
+        });
+
+        // Player clicks
+        socket.on('click', (col) => {
+            if (
+                clients.indexOf(socket.id) !== currentPlayer - 1 ||
+                gameFinished
+            ) {
+                return;
+            }
+
+            var row = placeTile(col);
+
+            if (row < 0) {
+                return;
+            }
+
+            io.sockets.emit('placeTile', {
+                index: col * 6 + row,
+                player: currentPlayer,
+            });
+
+            var checkPositions = [];
+            var colPositions = getColPositions(col);
+            var rowPositions = getRowPositions(row);
+            var topDiagonal = getTopDiagonal(col, row);
+            var bottomDiagonal = getBottomDiagonal(col, row);
+
+            checkPositions.push(colPositions);
+            checkPositions.push(rowPositions);
+            checkPositions.push(topDiagonal);
+            checkPositions.push(bottomDiagonal);
+
+            if (!checkVictory(checkPositions, 0)) {
+                switchTurn();
+            } else {
+                gameFinished = true;
+                console.log('Player ' + currentPlayer + ' won!');
+            }
         });
     });
 
-    const PORT = process.env.PORT || 3000;
-
     server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-    setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+    function init() {
+        currentPlayer = 1;
+        gameFinished = false;
+        var n = 42;
+        while (n > 0) {
+            gameBoard.push(0);
+            n--;
+        }
+    }
+
+    function placeTile(col) {
+        for (var i = (col + 1) * 6 - 1; i >= (col + 1) * 6 - 6; i--) {
+            if (gameBoard[i] == 0) {
+                gameBoard[i] = currentPlayer;
+                return i - col * 6;
+            }
+        }
+        return -1;
+    }
+
+    function getColPositions(col) {
+        var positions = [];
+        for (i = col * 6; i <= col * 6 + 5; i++) positions.push(gameBoard[i]);
+        return positions;
+    }
+    function getRowPositions(row) {
+        var positions = [];
+        for (var i = 0; i <= 6; i++) {
+            positions.push(gameBoard[i * 6 + row]);
+        }
+        return positions;
+    }
+    function getTopDiagonal(col, row) {
+        var positions = [];
+
+        // Get top left position of diagonal
+        while (col > 0 && row > 0) {
+            col--;
+            row--;
+        }
+
+        // Add positions diagonally down
+        while (row <= 5 && col <= 6) {
+            positions.push(gameBoard[col * 6 + row]);
+            row++;
+            col++;
+        }
+        return positions;
+    }
+    function getBottomDiagonal(col, row) {
+        var positions = [];
+
+        // Get bottom left position of diagonal
+        while (col > 0 && row < 5) {
+            col--;
+            row++;
+        }
+
+        // Add positions diagonally up
+        while (row >= 0 && col <= 6) {
+            positions.push(gameBoard[col * 6 + row]);
+            row--;
+            col++;
+        }
+        return positions;
+    }
+
+    function checkVictory(checkPositions, index) {
+        if (index >= checkPositions.length) {
+            return false;
+        } else {
+            var count = 0;
+            var winPositions = [];
+
+            for (var i = 0; i < checkPositions[index].length; i++) {
+                var current = checkPositions[index][i];
+
+                if (current === currentPlayer) {
+                    // console.log('current', current);
+                    // console.log('currentPlayer', currentPlayer);
+                    winPositions.push(current);
+                    count++;
+                    if (count >= 4) {
+                        return true;
+                    }
+                } else {
+                    count = 0;
+                    winPositions = [];
+                }
+            }
+            return checkVictory(checkPositions, index + 1);
+        }
+    }
+
+    function switchTurn() {
+        if (currentPlayer === 1) {
+            currentPlayer++;
+            io.to(clients[0]).emit('turn', false);
+            io.to(clients[1]).emit('turn', true);
+        } else {
+            currentPlayer = 1;
+            io.to(clients[0]).emit('turn', true);
+            io.to(clients[1]).emit('turn', false);
+        }
+    }
 })();
